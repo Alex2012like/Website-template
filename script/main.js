19e5125aea3df6dc19c4b65d1450b845b36d011e
alex2012like <oleksandrternavskij812@gmail.com>
2025-04-05 21:13:17 +0300

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,1 +1,6 @@
+const mainElement = document.getElementById('main');
+const clickMessageDiv = document.getElementById('message');
 
+mainElement.addEventListener('click', () => {
+  clickMessageDiv.textContent = 'You clicked in <main>';
+});
